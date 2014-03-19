acd644f51b9aa8baaaa0e0dc4dc1769faac9f189
Igor Kishik <ikishik@gmail.com>
2014-03-19 18:00:00 +0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ParraShopCom::Application.config.secret_key_base = '1b0a2aa386dcf8ec2e56cc64b7d070cb231285e6ff6496ed12a014a2e69a9c1f6eb43fbc38b3a3b4f13ea463e4f7a2a0e245e65ebdb517c14c30ba1bf5b19277'
