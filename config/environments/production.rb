3f8a0c93573b7a2e27c793129edf0de79bc2979f
Igor Kishik <ikishik@gmail.com>
2014-03-24 01:14:47 +0400

    Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 ParraShopCom::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => 'http://parrashop.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://www.parra-shop.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -20,14 +20,15 @@ ParraShopCom::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+  config.assets.initialize_on_precompile = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
