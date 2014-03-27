ebb9c1bb87471f60624389f80b0542503327ea7c
Igor Kishik <ikishik@gmail.com>
2014-03-27 14:23:23 +0400

    Fix

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -6,7 +6,7 @@ class CatalogController < ApplicationController
      session[:color] = nil
      
      @category = Category.find_by_scode(params[:category_scode])
-     @title = @category.s_title
+     #@title = @category.s_title
    rescue ActiveRecord::RecordNotFound
      logger.error("Попытка доступа к несуществующей категории #{params[:category_id]}")
      redirect_to_index("Неверный идентификатор категории")
