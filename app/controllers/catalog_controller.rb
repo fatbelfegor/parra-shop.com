c50fbb8418b8b9947d353924f5097d279d6764bf
Igor Kishik <ikishik@gmail.com>
2014-05-11 17:24:19 +0400

    Fix

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -2,14 +2,22 @@
 # encoding: utf-8
 
 class CatalogController < ApplicationController
-  def index
-    @category = Category.find_by_scode('bella')
-    #@title = @category.s_title
-    if @category
-      @products = @category.products
-    else
+    def index
+    begin
+      session[:proption] = nil
+      session[:prsize] = nil
+      session[:color] = nil
+      
+      @category = Category.find_by_scode(params[:category_scode])
+      #@title = @category.s_title
+    rescue ActiveRecord::RecordNotFound
       logger.error("Попытка доступа к несуществующей категории #{params[:category_id]}")
       redirect_to_index("Неверный идентификатор категории")
+    else
+      if(@category)
+        @products = @category.products
+      end
+ 
+      end
     end
   end
-end
