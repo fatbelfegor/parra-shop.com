e64f2b7092b17fe1a90c5d909604e8ac00baeb93
Roma <romadzao@gmail.com>
2014-06-03 01:05:04 +0300

    glavnaya

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,9 @@
 class MainController < ApplicationController
   def index
     @products = Product.all
+    @cat1 = Product.find :all, conditions: {category_id: 1}, limit: 5, order: 'created_at asc'
+    @cat2 = Product.find :all, conditions: {category_id: 2}, limit: 5, order: 'created_at asc'
+    @cat3 = Product.find :all, limit: 10, order: 'RANDOM()'
   end
   
   def main
