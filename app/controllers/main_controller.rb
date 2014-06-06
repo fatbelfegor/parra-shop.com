9f344fdb504c220761c5e26ec75136a62497bc78
Roma <romadzao@gmail.com>
2014-06-06 03:17:03 +0300

    commit

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,7 @@ class MainController < ApplicationController
   def index
     @products = Product.all
     @cat1 = Category.find_by_scode('bella').products.order('created_at asc').limit(5)
-    @cat2 = Category.find_by_name('style').products.order('created_at asc').limit(5)
+    @cat2 = Category.find_by_scode('style').products.order('created_at asc').limit(5)
     @cat3 = Category.find_by_name('Диваны').products.order('created_at asc').limit(10)
   end
   
