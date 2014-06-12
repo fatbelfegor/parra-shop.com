3df69d75d64805dc19de7f6a8b07ea713bbe8a75
Roma <romadzao@gmail.com>
2014-06-12 16:17:49 +0300

    texturesScode

--- app/controllers/prcolors_controller.rb
+++ app/controllers/prcolors_controller.rb
@@ -15,7 +15,7 @@ class PrcolorsController < ApplicationController
       Product.find_by_scode(params[:copy_scode]).prcolors.each do |p|
         @prcolor = Prcolor.create(product_id: prcolor_params[:product_id], scode: @productScode+'_'+p.scode, name: p.name, price: p.price, description: p.description, images: p.images)
         p.textures.each do |t|
-          Texture.create(prcolor_id: @prcolor.id, name: t[:name], scode: @productScode+t[:scode], price: t[:price], image: t[:image])
+          Texture.create(prcolor_id: @prcolor.id, name: t[:name], scode: t[:scode], price: t[:price], image: t[:image])
         end
       end
     else
