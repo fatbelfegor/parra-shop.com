cac146ceace1adf551e2deaa0c86f4b6efac9859
Roma <romadzao@gmail.com>
2014-06-14 14:24:46 +0300

    pass

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324111203) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "articles", force: true do |t|
-    t.string   "name"
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140423182542) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
