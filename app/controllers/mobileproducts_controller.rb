a83bacb869b473f539057aad7f3e07d5582c2fb9
Romaboy <romadzao@gmail.com>
2017-03-01 17:46:03 +0200

    commit

--- app/controllers/mobileproducts_controller.rb
+++ app/controllers/mobileproducts_controller.rb
@@ -1,8 +1,23 @@
 class MobileproductsController < ApplicationController
   
   def index
-    @products = Product.joins(:categories).where("categories.isMobile = true").distinct
-    
+    products = Product.select(:id, :category_id, :name, :shortdesk, :description, :price, :position, :updated_at).includes(:product_images).joins(:categories).where("categories.isMobile = true").distinct
+    result = []
+    for p in products
+    	result << {
+    		id: p.id,
+    		category_id: p.category_id,
+    		name: p.name,
+    		shortdesk: p.shortdesk,
+    		description: p.description,
+    		price: p.price,
+    		position: p.position,
+    		updated_at: p.updated_at,
+    		images: p.product_images.select(:image).map{|pi| pi.image}.join(',')
+    	}
+    end
+    render json: result
+
     # respond_to do |format|
 #         format.html { render :index }
 #         format.json { render :json =>  @categoriesmobile.to_json(:include => [:parent])}
