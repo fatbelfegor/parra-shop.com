967daea32c5015f7fc14f3b3831b69308dd18626
Romaboy <romadzao@gmail.com>
2017-05-29 20:27:40 +0300

    m

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,17 +2,17 @@ class Mailer < ActionMailer::Base
 
 	def partner_request(params)
 		@data = params
-		mail(to: 'tsv169@gmail.com', subject: 'Заявка на сотрудничество.')
+		mail(to: ['tsv169@gmail.com', 'kas1082@yandex.ru'], subject: 'Заявка на сотрудничество.')
 	end
 
 	def order_project(params)
 		@data = params
-		mail(to: 'tsv169@gmail.com', subject: 'Заказ дизайн проекта.')
+		mail(to: ['tsv169@gmail.com', 'kas1082@yandex.ru'], subject: 'Заказ дизайн проекта.')
 	end
 
 	def order_credit(params)
 		@data = params
-		mail(to: 'tsv169@gmail.com', subject: 'Заказ кредита.')
+		mail(to: ['tsv169@gmail.com', 'kas1082@yandex.ru'], subject: 'Заказ кредита.')
 	end
 
 end
